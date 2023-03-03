a5a0a0650fe8e93063bdcf849762f649f817cd2e
Cabanero11 <anruigui@upv.edu.es>
2023-03-03 20:05:47 +0100

    Agregrando fisicas y cinematica con el Hammer y cosas chulas
    Cosa chula

---
+++ Assets/Physics/Scrips/MoverCapsula.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MoverCapsula : MonoBehaviour
+{
+    
+    [Header("Cosas a lanzar")]
+    public GameObject capsule;
+    public float moveForce = 10f; // Force applied to the ball
+
+    private Rigidbody rb;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        rb = capsule.GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 launchDirection = mainCamera.transform.position - transform.position;
+            rb.AddForce(launchDirection.normalized * moveForce, ForceMode.Impulse);
+        }
+    }
+}
